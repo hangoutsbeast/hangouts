057b72ea9556f5001287780072a3dc1b976dc7ce
hangoutsbeast <39258977+hangoutsbeast@users.noreply.github.com>
2018-05-16 23:39:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8">
+	<title>Hangouts with Vishal</title>
+	<link rel="stylesheet/less" type="text/css" href="css/style.less">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+	<script src="js/less.js"></script>
+</head>
+<body>
+	<div class="wrapper">
+		<header class="header">
+			<div class="banner">
+				<div class="top-strip">
+					<div class="container">
+						<span>Category :</span>
+						<ul>
+							<li><a href="#">hello</a></li>
+							<li><a href="#">hello</a></li>
+							<li><a href="#">hello</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="logo">
+					<h1><span>Hangouts</span> <span>With</span> <span>Vishal</span></h1>
+				</div>
+				<div class="caption">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus repellat sed odit aperiam, minima suscipit doloremque, tempore, labore alias optio placeat quos molestiae, neque consectetur ab asperiores voluptatibus ipsa deleniti!</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus repellat sed odit aperiam, minima suscipit doloremque, tempore, labore alias optio placeat quos molestiae, neque consectetur ab asperiores voluptatibus ipsa deleniti!</p>
+				</div>
+			</div>
+			<div class="left">
+					<ul>
+						<li><a class="home" href="#"><span>Home</span> <i class="fa fa-home"></i></a></li>
+						<li><a class="images" href="#"><span>Images</span> <i class="fa fa-image"></i></a></li>
+						<li><a class="video" href="#"><span>Videos</span> <i class="fa fa-video-camera"></i></a></li>
+						<li><a class="blog" href="#"><span>Blog</span> <i class="fa fa-edge"></i></a></li>
+						<li><a class="contact" href="#"><span>Contact Us</span> <i class="fa fa-address-book"></i></a></li>
+					</ul>
+			</div>
+			<div class="right">
+					<ul>
+						<li><a class="home" href="#"><i class="fa fa-facebook-square"></i></a></li>
+						<li><a class="about" href="#"><i class="fa fa-twitter-square"></i></a></li>
+						<li><a class="images" href="#"><i class="fa fa-instagram"></i></a></li>
+						<li><a class="video" href="#"><i class="fa fa-youtube"></i></a></li>
+						<li><a class="blog" href="#"><i class="fa fa-rss"></i></a></li>
+					</ul>
+				
+			</div>
+		</header>
+	</div>
+</body>
+</html>
